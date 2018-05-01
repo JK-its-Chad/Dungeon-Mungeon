2c2d808c6e3001959ff06b577d4ca13bbfc21543
Vexlewolf <37556493+Vexlewolf@users.noreply.github.com>
2018-05-01 15:40:20 -0400

    Only for Connor to see

---
+++ Assets/Scripts/MainMenu/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : PlayerController {
+
+    Button Play;
+    Button Options;
+    Button Quit;
+    Button Back;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+
+    public override void Horizontal(float value)
+    {
+       
+    }
+
+    public override void Vertical(float value)
+    {
+        
+    }
+}
