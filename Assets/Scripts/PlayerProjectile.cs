520a26df809012badc33e0311076c369f3c3c201
SomeoneStrange <37556487+JK-its-Chad@users.noreply.github.com>
2018-05-03 16:26:31 -0400

    No more derp textures
    read title

---
+++ Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : Actor
+{
+
+    public float damageAmount = 10.0f;
+    public float movementSpeed = 20f;
+    public float lifetime = 2f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.velocity = transform.forward * movementSpeed;
+        rb.useGravity = false;
+        Destroy(gameObject, lifetime);
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            return;
+        }
+        Actor OtherActor = other.gameObject.GetComponentInParent<Actor>();
+        if (OtherActor)
+        {
+            OtherActor.TakeDamage(this, damageAmount, new DamageEventInfo(typeof(ProjectileDamageType)), Owner);
+        }
+        OnDeath();
+    }
+
+    public virtual void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+
+}
