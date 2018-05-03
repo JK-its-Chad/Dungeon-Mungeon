bd31594eb00800c9c01a409fc5c6beb0e3126d28
Vexlewolf <37556493+Vexlewolf@users.noreply.github.com>
2018-05-03 16:54:55 -0400

    SWEEEEEEEETTTTT VICTORY!!!!!
    Victory,SaxSolo fix

--- Assets/Scripts/BossRoom.cs
+++ Assets/Scripts/BossRoom.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class BossRoom : MonoBehaviour {
 
     public GameObject turret1, turret2, turret3, turret4;
@@ -13,6 +13,7 @@ public class BossRoom : MonoBehaviour {
 
         if (!turret1 && !turret2 && !turret3 && !turret4)
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             FindObjectOfType<AudioManager>().stop("Theme");
             FindObjectOfType<AudioManager>().play("SaxSolo");
         }
